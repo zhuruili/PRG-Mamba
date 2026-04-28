3fa2d6ca004db8ef35a23ee79fb2b390ce30960b
RookieNoob <lizhurui20040305@outlook.com>
2026-04-28 19:02:43 +0800

    update：训练参数更新

--- train_script.sh
+++ train_script.sh
@@ -3,11 +3,11 @@ python main_train.py \
 --ann_path /data/serverC/Projects/R2Gen-Mamba/datasets/UAPD/annotations-split.json \
 --dataset_name UAPD \
 --max_seq_length 100 \
---threshold 10 \
---batch_size 64 \
+--threshold 3 \
+--batch_size 32 \
 --epochs 100 \
 --save_dir results/UAPD \
---step_size 1 \
+--step_size 10 \
 --gamma 0.8 \
 --d_vf 512 \
 --visual_mode vmamba_swin \
